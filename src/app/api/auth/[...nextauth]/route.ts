e639c24be7a08fa48b9fb21faf221594ce8149a2
jen <jboucher154@gmail.com>
2023-05-14 16:06:28 +0300

    add inprogress jwt callback

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -28,19 +28,19 @@ import FortyTwoProvider from 'next-auth/providers/42-school'
 // 	}
 
 
-const generateURI = async ({ userId }: any) => {
-	const { HEXAGON_URL } = process.env
-	const result = await fetch(
-	  `${HEXAGON_URL}`,
-	  {
-		headers: {
-		  Accept: 'application/json'
-		}
-	  }
-	)
-	const data = await result.json()
-	return data.url
-  }
+// const generateURI = async ({ userId }: any) => {
+// 	const { HEXAGON_URL } = process.env
+// 	const result = await fetch(
+// 	  `${HEXAGON_URL}`,
+// 	  {
+// 		headers: {
+// 		  Accept: 'application/json'
+// 		}
+// 	  }
+// 	)
+// 	const data = await result.json()
+// 	return data.url
+//   }
 
 // can include this in the authOptions if we want to override the default signin page, value of  '/signin' = route to our page
 // 	pages: {
@@ -62,30 +62,32 @@ const authOptions = {
 	],
 	pages: {
 		signIn: 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-849602b1df456aaa42544c35b51928d82107ae14ecab309d2cfe48dd3cb5ccf5&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fsignin&response_type=code'
-	}
-	// callbacks: {
-	// 	async session({ session, token }: any) {
-	// 	  // Send accessToken to the client so we can use it later on
-	// 	  // eslint-disable-next-line no-param-reassign
-	// 	  session.accessToken = token.accessToken
-	// 	  session.login = token.login
-    //  	  session.user_id = token.user_id
-	// 	  session.link = generateURI(session.user_id)
-	// 	  return session
-	// 	},
-	// 	async jwt({ token, account }: any) {
-	// 	  if (account) {
-	// 		// Save access token to JWT token for later use
-	// 		// eslint-disable-next-line no-param-reassign
-	// 		token.accessToken = account.access_token
-	// 		// eslint-disable-next-line no-param-reassign
-	// 		token.refreshToken = account.refresh_token
-	// 	  }
-	// 	  return token
-	// 	}
-	//   }
+	},
+	callbacks: {
+		async jwt({ token, profile }: any) {
+			if (profile) {
+			  // Save access token to JWT token for later use
+			  // eslint-disable-next-line no-param-reassign
+			  token.user_id = profile.user_id
+			  // eslint-disable-next-line no-param-reassign
+			  token.login = profile.login
+			}
+			return token
+		  },
+		async session({ session, token }: any) {
+			delete session.user
+		  // Send accessToken to the client so we can use it later on
+		  // eslint-disable-next-line no-param-reassign
+			// session.accessToken = token.accessToken
+			session.login = token.login
+			session.user_id = token.user_id
+			session.link = process.env.HEXAGON_URL //generateURI(session.user_id)
+			return session
+		}
+	  }
 }
 
 const handler = NextAuth(authOptions)
 
 export {handler as GET, handler as POST}
+
