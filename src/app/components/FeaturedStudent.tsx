279bee8f465ddf8b46b5ec967aa11e3b806fb4ce
jen <jboucher154@gmail.com>
2023-05-15 10:50:13 +0300

    add Featured Student componenet

---
+++ src/app/components/FeaturedStudent.tsx
@@ -0,0 +1,30 @@
+'use client'
+import info from '../../../info.json';
+import { Profile } from "../../../typings";
+import { useState, useEffect } from 'react';
+
+const getRandomProfile = () => {
+	const profileIndex = (Math.floor(Math.random() * info.length));
+	console.log(profileIndex);//
+	return info[profileIndex];
+}
+
+const FeaturedStudent = () => {
+	const [featured, setFeatured] = useState<Profile | null>(null);
+	
+	useEffect(() => {
+		const profile = getRandomProfile();
+		setFeatured(profile);
+	}, [])
+	return (
+		<section>
+			<div>
+				<h3>Featured Student</h3>
+				<p>Name: {featured?.name}</p>
+				<p>Email: {featured?.email}</p>
+			</div>
+		</section>
+	)
+}
+
+export default FeaturedStudent
